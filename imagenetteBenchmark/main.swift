185626497b7c2b8de6bd5ea8edc714784d4eae7e
Ayush Agrawal <f20170604@goa.bits-pilani.ac.in>
2020-05-28 08:59:16 +0530

    Initial Commit

---
+++ imagenetteBenchmark/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  imagenetteBenchmark
+//
+//  Created by Ayush Agrawal on 28/05/20.
+//  Copyright © 2020 Ayush Agrawal. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
